a0b357cebf1c3ae95aa43547801308ce91ade9f3
Austin Nze <austinnze1000@gmail.com>
2022-09-20 21:57:17 +0100

    first commit

---
+++ vadidation.js
@@ -0,0 +1,12 @@
+// Code for valdiation of user emailfrom the html form
+const validEmails = {
+  email1: "austinnze1000@gmail.com",
+  email2: "austranking@gmaii.com",
+};
+
+const d = new Date();
+const button = document.getElementById("date")
+button.addEventListener("click", ()=>{
+    alert(d)
+}
+)
